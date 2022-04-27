cf469703827b967169a25c734b63fd46e8e04d4a
Facundo Alemandi <facuinstituto10@gmail.com>
2022-04-27 08:17:40 -0300

    validacion de email

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Creator from "./Components/Creator";
 import Email from "./Components/Email";
 import Ilustration from "./Components/Ilustration";
@@ -7,18 +8,39 @@ import Suscribe from "./Components/Suscribe";
 
 
 
+const expresiones = {
+ 
+  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 
+}
 
 
 function App() {
 
 
+   const [onChangeMail, setOnChangeMail] = useState({campo: '', valido: null})
+
+  const verifyMail = () => {
+   if(expresiones.correo){
+      if(expresiones.correo.test(onChangeMail.campo)){
+        setOnChangeMail({...onChangeMail, valido:'true'})
+      }else{
+        setOnChangeMail({...onChangeMail, valido:'false'})
+      }
+    }
+  }
 
   return (
     <>
        <Logo/>
        <Suscribe/>
-       <Email/>
+       <Email
+         verifyMail={verifyMail}
+         onChangeMail={onChangeMail}
+         setOnChangeMail={setOnChangeMail}
+         valido={onChangeMail.valido}
+         error={onChangeMail.valido}
+       />
        <Ilustration/>
        <Redes/>
        <Creator/>
--- src/Components/Email.js
+++ src/Components/Email.js
@@ -1,19 +1,42 @@
-import React from 'react'
-import '../styles/Email.css'
+import React from "react";
+import "../styles/Email.css";
 
+const Email = ({ verifyMail, onChangeMail, setOnChangeMail, valido, error}) => {
 
-const Email = ({verifyMail}) => {
-
-
+    const onChangeEmail = (e) => {
+      setOnChangeMail({...onChangeMail, campo:e.target.value });
+    }
+   
+     const isTrue = (text) => {
+       if(valido === 'true'){
+         return( text )
+       }
+     }
+     const isFalse = (text) => {
+       if(error === 'false'){
+         return( text )
+       }
+     }
+    
 
   return (
-     <>
-       <section className='email_container'>
-           <input type='email' placeholder='Enter Your Email' />
-           <button onClick={verifyMail}> Notify Me  </button>
-       </section>
-     </>
-  )
-}
+    <>
+      <section className="email_container">
+        <input
+          type="email"
+          placeholder="Enter Your Email"
+          value={onChangeMail.campo}
+          onChange={onChangeEmail}
+        />
+      
+        <button onClick={verifyMail}> Notify Me </button>
+      </section>
+      <section>
+      <p>{isTrue('hola')}</p>
+      <p>{isFalse('hola por 2')}</p>
+      </section>
+    </>
+  );
+};
 
-export default Email
+export default Email;
